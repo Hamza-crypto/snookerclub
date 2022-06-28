8cca7d2455edb8225c4c4aff22465cda1cec03e3
hamza-crypto <6793sidique@gmail.com>
2022-06-28 16:56:21 +0500

    helper function added

--- app/helpers.php
+++ app/helpers.php
@@ -27,8 +27,7 @@ function get_player_name($id ) {
 
 function get_img_url($key)
 {
-    //check if key contains http
-    if (strpos($key, 'https') !== false) {
+    if (str_contains($key, 'https')) {
         return $key;
     } else {
         return Storage::disk('s3')->url($key);
