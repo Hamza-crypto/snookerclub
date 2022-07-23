9b52d4528bbd40f70b9e199f4fb1977db83d6366
hamza-crypto <6793sidique@gmail.com>
2022-07-24 00:09:38 +0500

    header title text changes

--- resources/views/pages/results/index.blade.php
+++ resources/views/pages/results/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,900&amp;display=swap">
-    <title>Title</title>
+    <title>SnookernPool</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <link rel="stylesheet" href="{{ asset('assets/front/css/results/style.css') }}" >
