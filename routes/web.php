9d4f2f3ae8d45201f46e60d980f0e17827bc67a2
Hamza <6793siddique@gmail.com>
2023-12-04 00:11:57 +0500

    a

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function() {
     dd('Confirmed');
+    //
 });
 
 Route::get('/', [PlayerHistory::class, 'index_front'])->name('homepage.front');
