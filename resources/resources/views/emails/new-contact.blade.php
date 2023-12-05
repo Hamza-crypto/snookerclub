f9c0cd7b7e4a670fc7fd2de77fa1ff6c2a24dedf
Hamza <6793siddique@gmail.com>
2023-12-06 03:01:52 +0500

    Renewed Files

--- resources/resources/views/emails/new-contact.blade.php
+++
@@ -1,9 +0,0 @@
-@component('mail::message')
-
-You just got a new contact request from {{ $data['name'] }} <br>
-Email: {{ $data['email'] }}<br>
-Message: {{ $data['message'] }}
-
-Thanks,<br>
-{{ config('app.name') }}
-@endcomponent
