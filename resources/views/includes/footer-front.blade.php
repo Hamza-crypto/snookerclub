8e2d3eada2f2298b3551a00f3f7beccdcfcaa949
Hamza <6793siddique@gmail.com>
2023-12-06 03:11:27 +0500

    Year update

--- resources/views/includes/footer-front.blade.php
+++ resources/views/includes/footer-front.blade.php
@@ -5,12 +5,13 @@
         <div class="container-fluid">
             <!--<a class="navbar-brand" id="footerlogo" href="#">Logo</a>-->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link footernav active" aria-current="page" href="{{ route('tournament.contact') }}">Contact</a>
+                    <a class="nav-link footernav active" aria-current="page"
+                        href="{{ route('tournament.contact') }}">Contact</a>
                     <a class="nav-link footernav" href="#">Privacy Policy</a>
                     <a class="nav-link footernav" href="#">Terms of service</a>
                 </div>
@@ -18,13 +19,13 @@
         </div>
     </nav>
     <p id="footer-text">18, Av Omar Ibn Al Khattab Agdal - Rabat | Phone: (+212) 6 21 30 78 78 | Email:
-        contact@snookernpool.com <br><br> Copyright © 2022 Snookernpool</p>
+        contact@snookernpool.com <br><br> Copyright © {{ date('Y') }} Snookernpool</p>
     <!-- <div class="copyright">© 2022 - SNOOKERNPOOL</div> -->
 </footer>
 
 
 <!-- Google tag (gtag.js) -->
-@if(env('APP_ENV') == 'production')
+@if (env('APP_ENV') == 'production')
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-78WVB8NY9B"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
@@ -38,4 +39,3 @@
         gtag('config', 'G-78WVB8NY9B');
     </script>
 @endif
-
